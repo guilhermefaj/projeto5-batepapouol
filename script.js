6aa3b52973d6c1d1fa1a694a42ab50b96d76d57b
guilhermefaj <figueiredoguiu@gmail.com>
2023-01-19 11:53:22 -0300

    #005 Axios.get funcional para uma única mensagem

--- script.js
+++ script.js
@@ -2,23 +2,9 @@
 
 let resposta;
 let erro;
+let horario;
 
-function exibirMensagem() {
-
-    const requisicaoMensagem = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-    requisicaoMensagem.then(processarResposta);
-
-    function processarResposta(resposta) {
-        console.log("Voltou a resposta");
-    }
-
-    let template = `
-            <li class="msg">
-                <span class="horario">${horario}</span>
-                <span class="texto">${mensagem}</span>
-            </li>
-    `
-}
+cadastrarUsuario();
 
 function cadastrarUsuario() {
     const nome = prompt("Olá, como você quer ser chamado?");
@@ -31,16 +17,54 @@ function cadastrarUsuario() {
     function deuCerto(resposta) {
         console.log(resposta.data);
         console.log("Voltou a resposta");
+
+        exibirMensagem();
     }
     console.log("Requisição enviada");
 
     function tratarErro(erro) {
         console.log("Status code: " + erro.response.status);
         console.log("Mensagem de erro: " + erro.response.data);
+        alert("temos um erro");
     }
 
-    exibirMensagem();
+
 }
 
-cadastrarUsuario();
 
+function exibirMensagem() {
+
+    const requisicaoMensagem = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    requisicaoMensagem.then(processarResposta);
+    requisicaoMensagem.catch(tratarErro);
+
+    function processarResposta(resposta) {
+        console.log(resposta.data);
+
+        let balaoMsg = document.querySelector("ul");
+
+        for (let i = 0; i < resposta.data.length; i++) {
+
+            let balao = resposta.data[i];
+
+            mensagem = resposta.data.text;
+            horario = resposta.data.time;
+            from = resposta.data.from;
+
+            balaoMsg.innerHTML = `
+            <li class="msg">
+                <span class="horario">${balao.time}</span>
+                <span class="from">${balao.from}</span>
+                <span class="texto">${balao.text}</span>
+            </li>
+            `
+            balaoMsg++
+        }
+
+    }
+    function tratarErro(erro) {
+        console.log("Status code: " + erro.response.status);
+        console.log("Mensagem de erro: " + erro.response.data);
+        alert("temos um erro");
+    }
+}
